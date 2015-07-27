c7c2d8017c50e9eeea0201c8e7ac224f22d05266
malnick <malnick@gmail..com>
2015-07-27 09:56:47 -0700

    it begins

---
+++ README.md
@@ -0,0 +1,2 @@
+# SRC:CLR On-Premise SCM Agent Puppet Module
+This module installs the SRC:CLR on-premise agent from docker hub
---
+++ manifests/params.pp
@@ -0,0 +1,8 @@
+class scm_agent::params {
+  $docker_registry_url            = undef
+  $image_name                     = 'scm_agent'
+  $version                        = undef
+  $docker_env                     = []
+  $use_docker                     = false
+
+}
