f74cd8c8c322ab3dd8fcf25321842e971534ad0c
malnick <malnick@gmail..com>
2015-07-31 14:35:03 -0700

    unzip nam

--- manifests/install.pp
+++ manifests/install.pp
@@ -16,14 +16,14 @@ class scm_agent::install (
 
   package {['wget','unzip','git']: ensure => present, }
 
-  scm_agent::remote_file { "${install_dir}/scm_agent_${version}.zip":
+  scm_agent::remote_file { "${install_dir}/srcclr-scm-agent-${version}.zip":
     source    => "${download_url}/${pkg_name}-${version}.zip",
     require   => [Package['wget','unzip','git'],File[$install_dir]],
     notify    => Exec['unzip'],
   }
 
   exec { 'unzip':
-    command       => "/usr/bin/unzip ${install_dir}/scm_agent_${version}.zip",
+    command       => "/usr/bin/unzip ${install_dir}/srcclr-scm-agent-_${version}.zip",
     creates       => "${install_dir}/srcclr-scm-agent-${version}",
   }
 }
